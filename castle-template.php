829df19c7c4792dd6a252cfc20e6628826f26dbc
Tumas2 <Zoulofheaven@hotmail.com>
2016-02-02 13:09:53 +0100

    Create castle-template.php

---
+++ castle-template.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+
+ * Template Name: CastleTemp
+
+ *
+
+ * @package WordPress
+
+ */
+
+ 
+
+
+
+get_header(); ?>
+
+
+
+<div id="primary" class="content-area">
+
+	<main id="main" class="site-main" role="main">
+
+	
+
+<?php listFolderFiles(); ?>
+
+
+
+	</main><!-- .site-main -->
+
+
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+
+
+</div><!-- .content-area -->
+
+
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
+
