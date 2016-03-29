a3a727d2972d4f9b55ba6088530bd9c5cc21af33
Jessica Martin <jesmrtn11@gmail.com>
2016-03-29 11:28:09 +0200

    Latest changes to get the style.css to work

--- onepress-child/functions.php
+++ onepress-child/functions.php
@@ -1,1 +1,9 @@
 <?php
+/**
+ * Enqueue scripts and styles.
+ */
+function onepress_child_scripts() {
+    wp_enqueue_style( 'onepress-child-style', get_template_directory_uri().'-child/style.css' );
+
+}
+add_action( 'wp_enqueue_scripts', 'onepress_child_scripts' );
