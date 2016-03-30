4dc55b7ace5b77ecf6659b328bcdf69915462726
Jessica Martin <jesmrtn11@gmail.com>
2016-03-30 13:17:08 +0200

    Enqueued function 'onepress_child_scripts'

--- onepress-child/functions.php
+++ onepress-child/functions.php
@@ -3,7 +3,10 @@
  * Enqueue scripts and styles.
  */
 function onepress_child_scripts() {
-    wp_enqueue_style( 'onepress-child-style', get_template_directory_uri().'/style.css' );
+
+    wp_enqueue_style( 'onepress-child-style', get_template_directory_uri() . '-child/style.css' );
 
 }
-add_action( 'wp_enqueue_scripts', 'onepress_child_scripts' );
+
+add_action( 'wp_enqueue_scripts', 'onepress_child_scripts', 9999);
+
